59fa7e4beacc26f857f03f88a37ce3fcabd6fab7
nikam-shreyas <shreyas.s.nikam@gmail.com>
2023-11-30 12:22:09 -0500

    Persist dark mode through the pages

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let isDarkMode = localStorage.getItem("ns-portfolio-darkMode");
+
+// If dark mode is not stored in a cookie, set it to false
+if (isDarkMode === null) {
+  isDarkMode = true;
+}
+
+// Update the CSS class based on the stored dark mode preference
+if (isDarkMode === "true") {
+  document.documentElement.setAttribute("data-bs-theme", "dark");
+  document
+    .getElementById("theme")
+    .setAttribute("class", "nav-link mt-1 fa-solid p-2 fa-moon ");
+} else {
+  document.documentElement.setAttribute("data-bs-theme", "light");
+  document
+    .getElementById("theme")
+    .setAttribute("class", "nav-link mt-1 fa-solid p-2 fa-sun");
+}
+
 function changeTheme() {
   let currentTheme = document.documentElement.getAttribute("data-bs-theme");
 
@@ -7,12 +27,14 @@ function changeTheme() {
     document
       .getElementById("theme")
       .setAttribute("class", "nav-link mt-1 fa-solid p-2 fa-moon ");
+    localStorage.setItem("ns-portfolio-darkMode", true);
   } else {
     // change dark theme to light
     document.documentElement.setAttribute("data-bs-theme", "light");
     document
       .getElementById("theme")
       .setAttribute("class", "nav-link mt-1 fa-solid p-2 fa-sun");
+    localStorage.setItem("ns-portfolio-darkMode", false);
   }
 }
 
